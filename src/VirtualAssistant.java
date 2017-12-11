3537e5b19f042c53310c937c8b010d106d5a764c
Gaurav Jagtap <hello@iamgaurav.com>
2017-12-10 22:46:10 -0500

    Init

---
+++ src/VirtualAssistant.java
@@ -0,0 +1,67 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import opennlp.tools.namefind.*;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
+import opennlp.tools.util.Span;
+
+public class VirtualAssistant {
+
+	public static void main(String[] args) throws IOException, ParseException {
+		//Loading the NER-person model 
+		InputStream inputStreamTokenizer = new FileInputStream("src/model/en-token.bin");
+	    TokenizerModel tokenModel = new TokenizerModel(inputStreamTokenizer); 
+		
+	    TokenizerME tokenizer = new TokenizerME(tokenModel); 
+	    Scanner keyword = new Scanner(System.in);
+	    System.out.println("Enter the sentence");
+	    String sentence = keyword.nextLine(); 
+	    
+	    
+	    String tokens[] = tokenizer.tokenize(sentence); 	
+	    
+	   
+	    InputStream inputStreamNameFinder = new 
+	    FileInputStream("src/model/en-ner-location.bin");       
+	    TokenNameFinderModel model = new TokenNameFinderModel(inputStreamNameFinder);
+	       
+	    NameFinderME nameFinder = new NameFinderME(model);       
+	       
+	    Span nameSpans[] = nameFinder.find(tokens);        
+	    String city = "Toronto";
+	    for(Span s: nameSpans)   {     
+	         city = tokens[s.getStart()];
+	     }
+	 
+	    if(!city.isEmpty()) {
+	    		System.out.println("This is the city: " + city);
+	    		Weather weather = new Weather(city);
+	    		
+	    		StringBuffer result = weather.getWeather();
+	    		// Parse Json data from the API
+	    		JSONParser parser = new JSONParser();
+	    		Object jsonObj = parser.parse(result.toString());
+	    		JSONObject jsonObject = (JSONObject) jsonObj;
+	    		
+	    		// Pick out weather information
+	    		JSONObject weather_info = (JSONObject) jsonObject.get("main");
+	    		
+	    		// Convert from kelvin to celsius
+	    		Double temp = (Double) weather_info.get("temp") - 273.15f;
+	    		Double temp_min = (Double) weather_info.get("temp_min") - 273.15f;
+	    		Double temp_max = (Double) weather_info.get("temp_max") - 273.15f;
+	    		// Display the Information
+	    		System.out.printf("%.2f is the average temperature. %.2f is the lowest temperature and %.2f is the highest temperature", temp, temp_min, temp_max);
+	    }
+	    
+	}
+
+}
---
+++ src/Weather.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class Weather {
+	private String city;
+	private String api = "73655885c3b1d211db2836f4ba8a3fb9";
+	
+	public Weather(String city) {
+		this.city = city;
+	}
+	
+	public StringBuffer getWeather() throws ClientProtocolException, IOException {
+		String url_parse = "http://api.openweathermap.org/data/2.5/weather?q="+ city +"&APPID="+ api + "&type=accurate"; 
+		 
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpGet request = new HttpGet(url_parse);
+		
+		HttpResponse response = client.execute(request);
+		
+		BufferedReader rd = new BufferedReader(
+			new InputStreamReader(response.getEntity().getContent()));
+
+		StringBuffer result = new StringBuffer();
+		String line = "";
+		try {
+			while ((line = rd.readLine()) != null) {
+				result.append(line);
+			}
+			return result;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	
+}
